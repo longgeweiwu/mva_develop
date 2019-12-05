3d4df400082fb712e6a8fc3b9726638b5a670ecd
NOPSMILE <153223193@qq.com>
2019-12-05 16:04:17 +0800

    初始化1.1.0

--- src/main/java/com/itcc/mva/entity/AgentScore.java
+++ src/main/java/com/itcc/mva/entity/AgentScore.java
@@ -1,5 +1,6 @@
 package com.itcc.mva.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModelProperty;
@@ -12,5 +13,29 @@ public class AgentScore {
     @ApiModelProperty(value = "id")
     @TableId(value = "PID")
     private String pid;
+
+    @ApiModelProperty(value = "录音编号")
+    @TableField(value = "RECORD_PID")
+    private String recordPid;
+
+    @ApiModelProperty(value = "工单编号")
+    @TableField(value = "WORKORDER_PID")
+    private String workOrderPid;
+
+    @ApiModelProperty(value = "组织编号")
+    @TableField(value = "DEPARTMENT_PID")
+    private String departmentPid;
+
+    @ApiModelProperty(value = "坐席编号")
+    @TableField(value = "AGENT_ID")
+    private String agentId;
+
+    @ApiModelProperty(value = "总得分")
+    @TableField(value = "TOTAL_SCORE")
+    private Integer totalScord;
+
+    @ApiModelProperty(value = "申诉状态  0 未申诉  1 已提交申诉  2 申诉通过  3 申诉驳回")
+    @TableField(value = "APPEAL_STATE")
+    private String appealState;
 
 }
--- src/main/java/com/itcc/mva/service/impl/AgentScoreServiceImpl.java
+++ src/main/java/com/itcc/mva/service/impl/AgentScoreServiceImpl.java
@@ -1,8 +1,20 @@
 package com.itcc.mva.service.impl;
 
+import com.itcc.mva.entity.AgentScore;
+import com.itcc.mva.mapper.AgentScoreMapper;
+import com.itcc.mva.service.IAgentScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class AgentScoreServiceImpl {
+import java.util.List;
 
+@Service
+public class AgentScoreServiceImpl implements IAgentScoreService {
+    @Autowired
+    private AgentScoreMapper agentScoreMapper;
+
+    @Override
+    public List<AgentScore> getAgentList() {
+        return agentScoreMapper.getList();
+    }
 }

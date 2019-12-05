1e5b8d77b7575b621392158b85cb4712cc2449be
NOPSMILE <153223193@qq.com>
2019-12-05 15:04:02 +0800

    初始化1.1.0

--- src/main/java/com/itcc/mva/entity/AgentScore.java
+++ src/main/java/com/itcc/mva/entity/AgentScore.java
@@ -2,12 +2,14 @@ package com.itcc.mva.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 @TableName(value="T_QC_AGENTSCORE")
 public class AgentScore {
 
+    @ApiModelProperty(value = "id")
     @TableId(value = "PID")
     private String pid;
 

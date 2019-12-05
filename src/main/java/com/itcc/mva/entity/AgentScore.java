08bdfcb9a400b984193dc46c6de52e20ed0a880c
NOPSMILE <153223193@qq.com>
2019-12-05 15:02:27 +0800

    初始化1.1.0

--- src/main/java/com/itcc/mva/entity/AgentScore.java
+++ src/main/java/com/itcc/mva/entity/AgentScore.java
@@ -2,8 +2,9 @@ package com.itcc.mva.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
-
+@Data
 @TableName(value="T_QC_AGENTSCORE")
 public class AgentScore {
 

d16e61a58831916a00dccc08e52bbf3619477c63
NOPSMILE <153223193@qq.com>
2019-12-06 17:30:58 +0800

    运用最新4.0.0版本

--- src/main/java/com/itcc/mva/MvaApplication.java
+++ src/main/java/com/itcc/mva/MvaApplication.java
@@ -1,12 +1,10 @@
 package com.itcc.mva;
 
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.itcc.mva.mapper")
-@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 @SpringBootApplication
 public class MvaApplication {
 
--- src/main/java/com/itcc/mva/common/config/ScheduledLockConfig.java
+++ src/main/java/com/itcc/mva/common/config/ScheduledLockConfig.java
@@ -2,17 +2,16 @@ package com.itcc.mva.common.config;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import net.javacrumbs.shedlock.spring.ScheduledLockConfiguration;
-import net.javacrumbs.shedlock.spring.ScheduledLockConfigurationBuilder;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.sql.DataSource;
-import java.time.Duration;
 
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "30s")
 public  class ScheduledLockConfig {
 
     @Bean
@@ -20,12 +19,4 @@ public  class ScheduledLockConfig {
         return new JdbcTemplateLockProvider(dataSource);
     }
 
-    @Bean
-    public ScheduledLockConfiguration scheduledLockConfiguration(LockProvider lockProvider) {
-        return ScheduledLockConfigurationBuilder
-                .withLockProvider(lockProvider)
-                .withPoolSize(10)
-                .withDefaultLockAtMostFor(Duration.ofMinutes(10))
-                .build();
-    }
 }

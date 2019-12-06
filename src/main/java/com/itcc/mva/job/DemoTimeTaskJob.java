9855bc0a73c04768302e7d07ca775e5a638f9c94
NOPSMILE <153223193@qq.com>
2019-12-06 15:08:46 +0800

    添加log

--- src/main/java/com/itcc/mva/job/DemoTimeTaskJob.java
+++ src/main/java/com/itcc/mva/job/DemoTimeTaskJob.java
@@ -2,6 +2,8 @@ package com.itcc.mva.job;
 
 import com.itcc.mva.common.utils.Constant;
 import net.javacrumbs.shedlock.core.SchedulerLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +11,13 @@ import java.util.Date;
 
 @Component
 public class DemoTimeTaskJob {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Scheduled(cron = "0/2 * * * * ?")
     @SchedulerLock(name = "TaskScheduler_scheduledTask", lockAtMostFor = Constant.lockAtMostForTime, lockAtLeastFor = Constant.lockAtLeastForTime)
     public void run()
     {
-        System.out.print(new Date().toString() + "\n");
+        logger.info(new Date().toString() + "\n");
+       // System.out.print(new Date().toString() + "\n");
     }
 }

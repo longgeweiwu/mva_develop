cf0605be7b19b5ee395e0fb3fba4b4882873c38f
NOPSMILE <153223193@qq.com>
2019-12-09 09:41:03 +0800

    mybatis相关

--- src/main/java/com/itcc/mva/common/config/MybatisPlusConfig.java
+++ src/main/java/com/itcc/mva/common/config/MybatisPlusConfig.java
@@ -1,13 +1,10 @@
 package com.itcc.mva.common.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Properties;
 
 @EnableTransactionManagement
 @ComponentScan
@@ -19,16 +16,4 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    /**
-     * 打印 sql
-     */
-    @Bean
-    public PerformanceInterceptor performanceInterceptor() {
-        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
-        //格式化sql语句
-        Properties properties = new Properties();
-        properties.setProperty("format", "true");
-        performanceInterceptor.setProperties(properties);
-        return performanceInterceptor;
-    }
 }

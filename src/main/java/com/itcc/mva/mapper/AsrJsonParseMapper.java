201c536b6a2378ce485483d90978a3787c770434
NOPSMILE <153223193@qq.com>
2019-12-11 09:54:42 +0800

    修改代码

---
+++ src/main/java/com/itcc/mva/mapper/AsrJsonParseMapper.java
@@ -0,0 +1,15 @@
+package com.itcc.mva.mapper;
+
+import com.itcc.mva.entity.IntelligentAsrEntity;
+
+import java.util.List;
+
+public interface AsrJsonParseMapper {
+    /**
+     *
+     * @param top 查询多少条
+     * @return
+     */
+    public List<IntelligentAsrEntity> queryPendingTopMapper(int top);
+
+}

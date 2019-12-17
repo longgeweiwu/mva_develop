8a404746f35274d9c268baafb80d39812f4031d2
NOPSMILE <153223193@qq.com>
2019-12-17 17:08:25 +0800

    添加文件限制

---
+++ src/main/java/com/itcc/mva/common/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.itcc.mva.common.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class  WebMvcConfig implements WebMvcConfigurer {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Bean(name = "multipartResolver")
+    public MultipartResolver multipartResolverConfig() {
+        logger.info("loading the config of multipartResolverConfig");
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("UTF-8");
+//resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+//        resolver.setResolveLazily(true);
+//        resolver.setMaxInMemorySize(40960);
+//上传文件大小 10M 10*1024*1024
+        resolver.setMaxUploadSize(10 * 1024 * 1024);
+        return resolver;
+    }
+}

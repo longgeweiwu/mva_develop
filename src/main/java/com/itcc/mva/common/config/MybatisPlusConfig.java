becbae7e442805f3634c24691588bd13008f6732
NOPSMILE <153223193@qq.com>
2019-12-18 10:03:07 +0800

    logger.info("加载分页插件");

--- src/main/java/com/itcc/mva/common/config/MybatisPlusConfig.java
+++ src/main/java/com/itcc/mva/common/config/MybatisPlusConfig.java
@@ -3,16 +3,22 @@ package com.itcc.mva.common.config;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Properties;
 
+@Configuration
 @EnableTransactionManagement
 @ComponentScan
-@MapperScan("com.itcc.mva.mapper.*")
+//@MapperScan("com.itcc.mva.mapper*")
 public class MybatisPlusConfig {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Bean
     public PaginationInterceptor paginationInterceptor() {
@@ -22,11 +28,13 @@ public class MybatisPlusConfig {
      * 打印 sql
      */
     @Bean
+    @Profile({"test"})// 设置 test 环境开启
     public PerformanceInterceptor performanceInterceptor() {
+        logger.info("加载分页插件");
         PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
         //格式化sql语句
         Properties properties = new Properties();
-        properties.setProperty("format", "false");
+        properties.setProperty("format", "true");
         performanceInterceptor.setProperties(properties);
         return performanceInterceptor;
     }

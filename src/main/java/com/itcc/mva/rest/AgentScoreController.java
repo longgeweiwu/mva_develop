1ca018a79b24fa7ed5a6ff623e2d9ff15690c31e
NOPSMILE <153223193@qq.com>
2019-12-24 16:19:09 +0800

    M 1

---
+++ src/main/java/com/itcc/mva/rest/AgentScoreController.java
@@ -0,0 +1,19 @@
+package com.itcc.mva.rest;
+
+import com.itcc.mva.service.IPushToMvaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("/verify")
+public class AgentScoreController {
+    @Autowired
+    private IPushToMvaService iPushToMvaService;
+
+    @GetMapping(value = "/illega", produces = { "application/xml;charset=UTF-8" })
+    public String queryMission(@RequestParam(value="session.sce.id") String id) {
+     return iPushToMvaService.illegalId(id);
+    }
+}

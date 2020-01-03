d098cb3ab71105297c99aaa1a85498d9080f328e
NOPSMILE <153223193@qq.com>
2020-01-03 09:22:07 +0800

    M 1

--- src/main/java/com/itcc/mva/service/impl/PushToMvaServiceImpl.java
+++ src/main/java/com/itcc/mva/service/impl/PushToMvaServiceImpl.java
@@ -185,7 +185,7 @@ public class PushToMvaServiceImpl implements IPushToMvaService {
             jsonObject.put("acceptItem", "");//问题属地（行政区划码）这行为空
             jsonObject.put("regAppealContent", quarkCallbackEntity.getIflyResult());//主要述求详情
             //录音路径还有问题，需要注意
-            jsonObject.put("regRecordFileUri", Constant.RECORDURL + quarkCallbackEntity.getFullPath().split("/")[3] + "/" + quarkCallbackEntity.getVoiceFilename());//录音文件地址
+            jsonObject.put("regRecordFileUri", Constant.RECORDURL + quarkCallbackEntity.getFullPath().split("/")[3] + "/" + quarkCallbackEntity.getVoiceFileName());//录音文件地址
 
 
             Map<String, Object> validSign = GenSign.getValidSign();

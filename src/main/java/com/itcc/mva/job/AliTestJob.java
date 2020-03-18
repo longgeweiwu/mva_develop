84c1588b8b0c8634dc5015cbc196cf1c08ee7020
NOPSMILE <153223193@qq.com>
2020-03-18 15:36:54 +0800

    M 1

--- src/main/java/com/itcc/mva/job/AliTestJob.java
+++ src/main/java/com/itcc/mva/job/AliTestJob.java
@@ -49,8 +49,8 @@ public class AliTestJob {
     @Autowired
     private AliTestMapper aliTestMapper;
 
-    @Scheduled(cron = "0/30 * * * * ?")
-    @SchedulerLock(name = "AliTestVHm", lockAtMostFor = "28s", lockAtLeastFor = "28s")
+    @Scheduled(cron = "0/10 * * * * ?")
+    @SchedulerLock(name = "AliTestVHm", lockAtMostFor = "8s", lockAtLeastFor = "8s")
     public void vocuPutRecordToAli() {
         List<String> name = getFileName();
         for(int i=0;i<name.size();i++) {
@@ -110,7 +110,7 @@ public class AliTestJob {
     @SchedulerLock(name = "AliTestGetVHmResult", lockAtMostFor = "28s", lockAtLeastFor = "28s")
     public void AliTestGetVHmResult() throws IOException {
 
-        List<AliTestEntity> aliTestEntities=aliTestMapper.selectList(new QueryWrapper<AliTestEntity>().isNotNull("ORITASKID").isNotNull("VMTASKID").isNotNull("VHMTASKID"));
+        List<AliTestEntity> aliTestEntities=aliTestMapper.selectList(new QueryWrapper<AliTestEntity>().isNotNull("ORITASKID").isNotNull("VMTASKID").isNotNull("VHMTASKID").isNotNull("VHM_RESULT").isNotNull("VM_RESULT").isNotNull("ORI_RESULT"));
 
         for(int j=0;j<aliTestEntities.size();j++){
 

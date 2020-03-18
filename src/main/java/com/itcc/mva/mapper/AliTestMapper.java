bfcf3878eb545c56e70828bedf1ebc5d240923dd
NOPSMILE <153223193@qq.com>
2020-03-18 15:18:43 +0800

    M 1

---
+++ src/main/java/com/itcc/mva/mapper/AliTestMapper.java
@@ -0,0 +1,13 @@
+package com.itcc.mva.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itcc.mva.entity.AliTestEntity;
+
+import java.util.List;
+
+/**
+ * @author whoami
+ */
+public interface AliTestMapper extends BaseMapper<AliTestEntity> {
+
+}

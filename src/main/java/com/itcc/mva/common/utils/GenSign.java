0cf81a35cefd6c945f00ca5383744da7a4bceba6
NOPSMILE <153223193@qq.com>
2021-01-27 16:03:36 +0800

    添加新方法

--- src/main/java/com/itcc/mva/common/utils/GenSign.java
+++ src/main/java/com/itcc/mva/common/utils/GenSign.java
@@ -30,4 +30,28 @@ public class GenSign {
         result.put("sign",sign);
         return result;
     }
+
+
+
+    public static Map<String,Object> getValidSign(String data){
+        Map<String, Object> result = new HashMap<String, Object>();
+        //appId
+        String appId = "incoming_phone";
+        //appSecret
+        String appSecret="0978cb19aea045518138ea5ef2c14075";
+        //加密盐值
+        String salt = "719a4fa475594f129b7ebfbdabd54369";
+        //当前时间戳
+        long t = System.currentTimeMillis();
+
+        //组装带加密的数据
+        String signStr ="appId"+appId+"t"+t+"appSecret"+appSecret
+                +"data"+data;
+        //进行加密
+        Md5DataSign md5Sign = new Md5DataSign();
+        String sign = md5Sign.sign(salt, signStr);
+        result.put("t",String.valueOf(t));
+        result.put("sign",sign);
+        return result;
+    }
 }
